b877970dc7de2391964787607cf20a63857c8baf
Voltstrike <mikailhusada@gmail.com>
2025-09-14 19:32:50 +0700

    Update Navbar change MyPortfolio to my name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
         {/* Logo */}
         <Link href="/" className="text-2xl font-bold text-primary">
-          MyPortfolio
+          Mikail Crito Husada
         </Link>
 
         {/* Desktop Menu */}
